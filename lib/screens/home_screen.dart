ac1e443a41add5b9c1e94f5da57391f26b6558f4
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-06-10 02:03:30 +0600

    changed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,10 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+      ),
+    );
   }
 }
